7422a45399f13c3fcbbef19405f87392fb643217
Frank LENORMAND <lenormf@gmail.com>
2018-08-22 09:08:17 +0300

    test: Add a second conflict into the merge

--- test.sh
+++ test.sh
@@ -18,18 +18,23 @@ cat
 dog
 octopus
 octocat
+
+other weird animals
+
+octobat
+eof
 EOF
 
     git add animals.txt
     git commit -m "Initial commit"
 
     git checkout -b octodog
-    sed -i s/octopus/octodog/ animals.txt
+    sed -i 's/octopus/octodog/; s/octobat/octoocto/' animals.txt
 
     git commit -am "Replace octopus with octodog"
 
     git checkout master
-    sed -i s/octopus/octoman/ animals.txt
+    sed -i 's/octopus/octoman/; s/octobat/octo/' animals.txt
 
     git commit -am "Replace octopus with an octoman"
 
